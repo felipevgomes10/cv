eacedf5cd27e50d078809dcfdce40774f1bb251f
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2022-11-21 17:23:13 -0300

    feat: create cv page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,75 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const fullText = "Felipe Gomes";
+
 const Home = () => {
+  const [text, setText] = useState("F");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setText(fullText.slice(0, text.length + 1));
+    }, 100);
+  }, [text]);
+
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      Felipe Gomes
-    </div>
+    <main className="flex justify-center items-center min-h-screen p-8 bg-gradient-to-br from-secondary to-primary bg-[length:300%] animate-bg-wave">
+      <section className="flex flex-col justify-center items-center p-[3.75rem] max-[500px]:p-[2rem] max-[400px]:p-[1rem] bg-bgTransparent w-[35rem] h-[42rem] rounded-lg shadow-primaryShadow">
+        <div className="overflow-hidden w-[18.75rem] h-[18.75rem] max-[500px]:w-[15rem] max-[500px]:h-[15rem] rounded-full border-4 border-textSecondary">
+          <Image
+            src="/profile.jpg"
+            alt="profile-picture"
+            width={300}
+            height={300}
+            placeholder="blur"
+            blurDataURL="/profile.jpg"
+          />
+        </div>
+        <h1 className="flex items-center gap-1 text-[2.5rem] max-[500px]:text-[2rem] uppercase font-bold text-textPrimary leading-[3.31rem] mt-[10px] mb-[5px] after:content-[''] after:block after:h-[2.25rem] after:w-[4px] after:bg-textPrimary after:animate-blink">
+          {text}
+        </h1>
+        <h2 className="text-[1.25rem] max-[500px]:text-[1rem] uppercase font-semibold text-textSecondary leading-[1.62rem] mb-[20px]">
+          Software developer
+        </h2>
+        <div className="flex justify-center items-center gap-[15px] bg-textPrimary rounded-md py-[0.75rem] px-[1.5rem] shadow-primaryShadow">
+          <a
+            href="https://github.com/felipevgomes10"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/github.svg"
+              alt="github-icon"
+              width={44}
+              height={46}
+              placeholder="blur"
+              blurDataURL="/github.svg"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/felipe-gomes-4b70221a8/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/linkedin.svg"
+              alt="linkedin-icon"
+              width={44}
+              height={46}
+              placeholder="blur"
+              blurDataURL="/linkedin.svg"
+            />
+          </a>
+        </div>
+        <a
+          href="/cv.pdf"
+          download
+          className="bg-primary text-textPrimary text-[1.12rem] max-[500px]:text-[1rem] leading-[1.68rem] font-semibold uppercase rounded-md py-[0.75rem] px-[1.5rem] shadow-primaryShadow mt-[20px]"
+        >
+          Download my cv
+        </a>
+      </section>
+    </main>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,34 @@ module.exports = {
         textPrimary: "#FFF2E1",
         textSecondary: "#F4E8D7",
         bgTransparent: "rgba(255, 255, 255, 0.15)",
-        primaryShadow: "rgba(102, 85, 41, 0.25)",
       },
       fontFamily: {
         primary: "IBM Plex Sans, sans-serif",
+      },
+      boxShadow: {
+        primaryShadow: "0 3px 3px rgba(102, 85, 41, 0.25)",
+      },
+      keyframes: {
+        "bg-wave": {
+          from: {
+            "background-position": "0%",
+          },
+          to: {
+            "background-position": "100%",
+          },
+        },
+        blink: {
+          from: {
+            opacity: 0,
+          },
+          to: {
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        "bg-wave": "bg-wave 1.8s linear infinite alternate",
+        blink: "blink .8s linear infinite",
       },
     },
   },
